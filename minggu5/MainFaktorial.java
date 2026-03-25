2b71d3469cfa8299c98e19301da8889d5bc59e85
Dairu Fatahillah <dairufatahillah60@gmail.com>
2026-03-25 17:00:54 +0700

    Percobaan 5.2.2

--- minggu5/MainFaktorial.java
+++ minggu5/MainFaktorial.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainFaktorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("masukan nilai");
+        System.out.print("masukan nilai: ");
         int nilai = input.nextInt();
 
         Faktorial fk = new Faktorial();
