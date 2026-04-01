6b2525ec6709b0dca9d4045d678fa76be2a666c6
Dairu Fatahillah <dairufatahillah60@gmail.com>
2026-04-01 10:02:47 +0700

    Percobaan 5.4.1

---
+++ minggu5/Sum.java
@@ -0,0 +1,30 @@
+package minggu5;
+
+public class Sum {
+    public int elemen;
+    public double keuntungan[];
+
+    public Sum(int el) {
+        elemen = el;
+        keuntungan = new double[el];
+    }
+
+    public double totalBF() {
+        double total = 0;
+        for (int i = 0; i < keuntungan.length; i++) {
+            total = total + keuntungan[i];
+        }
+        return total;
+    }
+
+    public double totalDC(double arr[], int l, int r) {
+        if (l == r) {
+            return arr[l];
+        } else {
+            int mid = (l + r) / 2;
+            double lsum = totalDC(arr, l, mid);
+            double rsum = totalDC(arr, mid + 1, r);
+            return lsum + rsum;
+        }
+    }
+}
